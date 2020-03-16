2e26e157f05bf682edae385a665bb032ca379e72
Busra Uludag <busrauludag13@gmail.com>
2020-03-16 21:40:45 +0300

    added svg to svg component package and used them

--- src/views/search.js
+++ src/views/search.js
@@ -4,6 +4,8 @@ import {Text, Button} from 'react-native';
 import BoxCenter from './../components/box-center';
 import Box from './../components/box';
 
+import {Bookmark} from '../components/icons';
+
 function SearchView({navigation}) {
   return (
     <BoxCenter>
@@ -13,6 +15,7 @@ function SearchView({navigation}) {
         onPress={() => navigation.navigate('Detail')}
       />
       <Box size={20} bg="blue" mt={20} />
+      <Bookmark width={50} height={50} stroke="black" />
     </BoxCenter>
   );
 }
