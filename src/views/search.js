958528e1d91e27f5685bf9a401939d4244c8b063
Busra Uludag <busrauludag13@gmail.com>
2020-04-08 22:20:27 +0300

    search page animations is done

--- src/views/search.js
+++ src/views/search.js
@@ -17,51 +17,63 @@ function SearchView() {
   React.useEffect(() => {
     if (isSearchFocus) {
       Animated.timing(heroHeight, {
-        toValue: 0,
-        duration: 500,
+        toValue: 52 + 32,
+        duration: 230,
       }).start();
     } else {
       Animated.timing(heroHeight, {
         toValue: 285,
-        duration: 500,
+        duration: 230,
       }).start();
     }
   }, [heroHeight, isSearchFocus]);
 
   useFocusEffect(
     React.useCallback(() => {
-      StatusBar.setBarStyle('light-content');
-    }, []),
+      StatusBar.setBarStyle(isSearchFocus ? 'dark-content' : 'light-content');
+    }, [isSearchFocus]),
   );
 
   return (
-    <Box as={SafeAreaView} bg="red" flex={1}>
-      <StatusBar barStyle="light-content" />
+    <Box as={SafeAreaView} bg={isSearchFocus ? 'softRed' : 'red'} flex={1}>
       {/* Header */}
       <Box
         as={Animated.View}
         position="relative"
         zIndex={1}
         height={heroHeight}>
+        {!isSearchFocus && (
+          <Box
+            as={ImageBackground}
+            source={bg}
+            style={{ width: '100%', height: '100%' }}>
+            {/* Logo */}
+            <Box flex={1} alignItems="center" justifyContent="center">
+              <Logo width={120} color="white" />
+            </Box>
+          </Box>
+        )}
+        {/* Search */}
         <Box
-          as={ImageBackground}
-          source={bg}
-          style={{ width: '100%', height: '100%' }}>
-          {/* Logo */}
-          <Box flex={1} alignItems="center" justifyContent="center">
-            <Logo width={120} color="white" />
-          </Box>
-          {/* Search */}
-          <Box width="100%" p={16} mb={-42}>
-            <Search onChangeFocus={status => setSearchFocus(status)} />
-          </Box>
+          position="absolute"
+          left={0}
+          bottom={isSearchFocus ? 0 : -42}
+          width="100%"
+          p={16}>
+          <Search onChangeFocus={status => setSearchFocus(status)} />
         </Box>
       </Box>
       {/* Content */}
-      <Box flex={1} bg="white" pt={26}>
-        <Box p={30} flex={1}>
-          <Text>Hello</Text>
-        </Box>
+      <Box flex={1} bg="white" pt={isSearchFocus ? 0 : 26}>
+        {isSearchFocus ? (
+          <Box p={30} flex={1}>
+            <Text>Gecmis</Text>
+          </Box>
+        ) : (
+          <Box p={30} flex={1}>
+            <Text>Oneri</Text>
+          </Box>
+        )}
       </Box>
     </Box>
   );
